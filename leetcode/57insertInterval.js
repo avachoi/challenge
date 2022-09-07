475af5ebade76425407a1685f6b21f17d347dbcb
avachoi <alexbchoi@gmail.com>
2022-09-07 12:08:24 -0400

    added 57

---
+++ leetcode/57insertInterval.js
@@ -0,0 +1,66 @@
+// 57. Insert Interval
+// You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+// Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+// Return intervals after the insertion.
+
+// Example 1:
+
+// Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+// Output: [[1,5],[6,9]]
+// Example 2:
+
+// Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// Output: [[1,2],[3,10],[12,16]]
+// Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+// Constraints:
+
+// 0 <= intervals.length <= 104
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 105
+// intervals is sorted by starti in ascending order.
+// newInterval.length == 2
+// 0 <= start <= end <= 105
+
+var insert = function (intervals, newInterval) {
+	if (intervals.length === 0) return [newInterval];
+	let array = [...intervals];
+	let newIn = [...newInterval];
+	let res = [];
+	let newInIn = false;
+	for (let i = 0; i < array.length; i++) {
+		let current = array[i];
+
+		if (current[1] >= newIn[0] && current[0] <= newIn[1]) {
+			//overlapping
+			newIn = [Math.min(current[0], newIn[0]), Math.max(current[1], newIn[1])];
+		} else {
+			//no overlapping
+			if (current[1] > newIn[0]) {
+				//current is bigger
+				console.log("i", i, newIn, array.slice(i));
+				res.push(newIn);
+				newInIn = true;
+				res.push(...array.slice(i));
+				return res;
+			} else {
+				//new is bigger
+				res.push(current);
+			}
+		}
+	}
+	if (!newInIn) {
+		res.push(newIn);
+	}
+	return res;
+};
+
+//if current & new is overlapping-> change new
+//if not overlapping->
+//if current is smaller -> push smaller
+//else push new and rest
+
+//[[1,5]], [2,3]   -> [[1,5]]
+//[], [2,3] -> [[2,3]]
